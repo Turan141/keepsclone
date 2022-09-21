5a1ca3f352ff45c41acf3a0c86a7abb5e490b534
Turan141 <darkhero-95@mail.ru>
2022-09-21 17:18:22 +0400

    git push

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from "react"
-import { Routes, Route } from "react-router-dom"
-import { Header } from "./components/Header/Header"
-import { SideBar } from "./components/Sidebar/Sidebar"
-import { Workspace } from "./components/Workspace/Workspace"
-import "./App.scss"
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Header } from './components/Header/Header'
+import { SideBar } from './components/Sidebar/Sidebar'
+import { Workspace } from './components/Workspace/Workspace'
+import './App.scss'
 
 function App() {
-	return (
-		<div className='App'>
-			<Header />
-			<div className='section_main'>
-				<SideBar />
-				<Routes>
-					<Route path='/' element={<Workspace />} />
-				</Routes>
-			</div>
-		</div>
-	)
+  return (
+    <div className="App">
+      <Header />
+      <div className="section_main">
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<Workspace />} />
+        </Routes>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./index.css"
-import App from "./App"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-import {
-	BrowserRouter,
-	Route,
-	Routes,
-} from "react-router-dom"
-import { Workspace } from "./components/Workspace/Workspace"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Workspace } from './components/Workspace/Workspace'
 
-const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
-)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
